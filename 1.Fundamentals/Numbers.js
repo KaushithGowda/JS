6efad79bec0a1b1b1a2de8f42324b46fa1bdbee0
KaushithGowda <koushith7@gmail.com>
2020-07-17 15:31:18 +0530

    fundamentals

---
+++ 1.Fundamentals/Numbers.js
@@ -0,0 +1,57 @@
+// Numbers in JS
+/* Number is a type of data type in JS */
+let number = 100;
+number2 = 2;
+
+sum = number + number2;
+sub = number - number2;
+mul = number * number2;
+div = number / number2;
+
+console.log(sum,sub,mul,div);
+
+//Modules in JS
+/* Module in JS is feature where JS returns remainder of two divided numbers */
+
+/* Module in JS can be used to find odd or even number */
+
+// Even number
+/* It is an even number when remainder is 0 after division */
+let mod1 = 8;
+mod2 = 2;
+
+mod = mod1 % mod2;
+
+console.log(mod);
+
+// Odd number
+/* It is an odd number when remainder is 1 after division */
+let mod3 = 9;
+mod4 = 2;
+
+mod_ = mod3 % mod4;
+
+console.log(mod_);
+
+// Basic operations to a declared variable
+let addition = number += 10;
+subtraction = number -= 10;
+multiplication = number *= 10;
+division = number /= 10;
+
+console.log(addition,subtraction, multiplication, division);
+
+let add = 10;
+add++;add++;
+
+sub = 10;
+sub--;sub--;
+
+multi = 10;
+// multi**;(this property does not apply to multiplication)
+
+divi = 10;
+// divi**;(this property does not apply to division)
+
+
+console.log(add,sub,multi,divi);
---
+++ 1.Fundamentals/StringConcatenation.js
@@ -0,0 +1,16 @@
+/* String Concatenation is adding of two or more string together */
+
+let firstName = "john";
+lastName = "peppar";
+
+console.log(firstName + " " + lastName);
+
+let first_Name = "john";
+lastName = "peppar";
+fullName = first_Name + " " + lastName
+console.log(fullName);
+
+let web = "google";
+url = "https://" + web + '.com'
+
+console.log(url);
