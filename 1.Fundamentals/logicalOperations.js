3e62c101c57211f5bf6535519f0c75448f638f15
KaushithGowda <koushith7@gmail.com>
2020-07-18 21:46:11 +0530

    Message

---
+++ 1.Fundamentals/logicalOperations.js
@@ -0,0 +1,28 @@
+//Logical operators
+
+//AND Operator(&&)
+let day = "monday";
+let money = 55;
+
+if(day === "friday" && money > 50 ){
+    
+    console.log("You are going out");
+}
+else{
+
+    console.log("You are not going out");
+}
+
+
+//OR Operator(||)
+let day2 = "monday";
+let money2 = 55;
+
+if(day2 === "friday" || money2 > 50 ){
+    
+    console.log("You are going out");
+}
+else{
+
+    console.log("You are not going out");
+}
